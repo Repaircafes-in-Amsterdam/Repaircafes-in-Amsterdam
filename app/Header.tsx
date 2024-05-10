5d81232037b5ec2f88bdd9170f0563ff46143381
peteruithoven <peter@metabolic.nl>
2024-05-10 14:06:10 +0200

    Add hamburger menu

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,12 +1,40 @@
+"use client";
 import Logo from "@/app/Logo.svg?react";
+import MenuIcon from "@/app/icons/Menu.svg?react";
 import Link from "next/link";
+import Menu from "./Menu";
+import { useRef, useState } from "react";
+import classes from "./classes";
 
 export default function Header() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   return (
-    <header className="flex bg-blue p-3">
-      <Link href="/">
-        <Logo />
-      </Link>
-    </header>
+    <>
+      <header className="relative flex items-center justify-between bg-blue p-3">
+        {/* Hidden overlay to dismiss menu */}
+        <div
+          className={classes(
+            "absolute left-0 top-0 z-10 h-screen w-screen",
+            !menuIsOpen && "hidden",
+          )}
+          onClick={() => setMenuIsOpen(false)}
+        ></div>
+        <Link href="/">
+          <Logo />
+        </Link>
+        <button
+          onClick={() => setMenuIsOpen(!menuIsOpen)}
+          className="cursor-pointer text-white"
+        >
+          <MenuIcon />
+        </button>
+      </header>
+      <div className="relative">
+        <Menu
+          isOpen={menuIsOpen}
+          onOpenChange={(isOpen) => setMenuIsOpen(isOpen)}
+        />
+      </div>
+    </>
   );
 }
---
+++ app/Menu.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+import ChevronRight from "@/app/icons/ChevronRight.svg?react";
+import classes from "./classes";
+
+function MenuItem({ href, children }: { href: string; children: ReactNode }) {
+  return (
+    <Link
+      href={href}
+      className="flex items-center justify-between gap-3 p-3 pr-2.5 font-medium text-blue"
+    >
+      {children}
+      <ChevronRight />
+    </Link>
+  );
+}
+
+import React, { forwardRef } from "react";
+const Menu = forwardRef<
+  HTMLElement,
+  { isOpen: boolean; onOpenChange: (isOpen: boolean) => void }
+>(({ isOpen, onOpenChange }, ref) => (
+  <nav
+    ref={ref}
+    className={classes(
+      "absolute z-20 w-full border-b-2 border-blue bg-white",
+      !isOpen && "hidden",
+    )}
+    onClick={() => onOpenChange(false)}
+  >
+    <MenuItem href="/">Aankomende</MenuItem>
+    <MenuItem href="/about">Over ons</MenuItem>
+  </nav>
+));
+
+Menu.displayName = "Menu";
+export default Menu;
