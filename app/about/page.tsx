d1f04f6d6d5b5b8935f4c0e3f1d300225e38d968
peteruithoven <peter@metabolic.nl>
2024-05-10 14:11:23 +0200

    Show amount of Repair Cafes on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import ChevronLeft from "@/app/icons/ChevronLeft.svg?react";
 import Mail from "@/app/icons/Mail.svg?react";
+import data from "@/data/data.json";
 
 export default function Page() {
+  const numRepairCafes = data.length;
+  console.log("numRepairCafes: ", numRepairCafes);
   return (
     <main className="flex shrink grow flex-col text-blue">
       <Link href="/" className="flex gap-3 p-3">
@@ -17,6 +20,9 @@ export default function Page() {
           makkelijker vindbaar te maken met hun openingstijden.
         </p>
         <p>
+          Tot dusver staan er {numRepairCafes} Repair Cafés op deze website.
+        </p>
+        <p>
           De informatie op deze website is Amsterdam specifiek, voor meer
           informatie over Repair Cafés in het algemeen zie{" "}
           <a
@@ -28,6 +34,7 @@ export default function Page() {
           </a>
           .
         </p>
+
         <p>
           Wil je een Repair Café toevoegen of is er informatie incorrect of
           onvolledig, dan stellen we een e-mail zeer op prijs.
