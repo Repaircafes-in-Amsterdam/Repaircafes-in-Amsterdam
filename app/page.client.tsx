c4cf315fa3234343b5dd530ad831769b19e1055f
peteruithoven <peter@metabolic.nl>
2024-05-25 22:43:02 +0200

    Separate client and server pages

---
+++ app/page.client.tsx
@@ -0,0 +1,17 @@
+"use client";
+import DistrictSelect from "./DistrictSelect";
+import OfficeHoursCheckbox from "./OfficeHoursCheckbox";
+import Upcoming from "./Upcoming";
+import { Event } from "./types";
+
+export default function Page({ events }: { events: Event[] }) {
+  return (
+    <div className="flex w-full max-w-body flex-col">
+      <div className="flex flex-wrap gap-x-3 gap-y-2 px-3 pt-3">
+        <DistrictSelect />
+        <OfficeHoursCheckbox />
+      </div>
+      <Upcoming events={events} />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
-import DistrictSelect from "./DistrictSelect";
-import OfficeHoursCheckbox from "./OfficeHoursCheckbox";
-import Upcoming from "./Upcoming";
 import getEvents from "./getEvents";
 import { Event } from "./types";
+import ClientPage from "./page.client";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomeServer() {
   const events: Event[] = getEvents();
-  return <HomeClient events={events} />;
-}
-
-function HomeClient({ events }: { events: Event[] }) {
-  return (
-    <div className="flex w-full max-w-body flex-col">
-      <div className="flex flex-wrap gap-x-3 gap-y-2 px-3 pt-3">
-        <DistrictSelect />
-        <OfficeHoursCheckbox />
-      </div>
-      <Upcoming events={events} />
-    </div>
-  );
+  return <ClientPage events={events} />;
 }
