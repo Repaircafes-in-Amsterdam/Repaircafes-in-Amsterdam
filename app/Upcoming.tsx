cb58626fcb5ebba03b36a583a195c120d5a7beae
peteruithoven <peter@metabolic.nl>
2024-06-02 13:10:12 +0200

    Only show hover style on hover supporting devices.

--- app/Upcoming.tsx
+++ app/Upcoming.tsx
@@ -51,7 +51,7 @@ export default function Upcoming({ events }: { events: Event[] }) {
               <li key={rc.slug}>
                 <Link
                   href={`cafe/${rc.slug}`}
-                  className="flex cursor-pointer items-center gap-3 bg-blue px-3 py-1.5 text-white hover:bg-orange hover:text-blue-600"
+                  className="flex cursor-pointer items-center gap-3 bg-blue px-3 py-1.5 text-white [@media(hover:hover)]:hover:bg-orange [@media(hover:hover)]:hover:text-blue-600"
                 >
                   <div className="flex grow flex-col">
                     <em className="font-semibold not-italic">{rc.name}</em>
