402ee03d9e9d180e3ce9b396834aa633f9b4ce62
peteruithoven <peter@metabolic.nl>
2024-07-20 20:28:26 +0200

    Add manifest

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Repair Cafes in Amsterdam",
+    short_name: "Repair Cafes in Amsterdam",
+    description:
+      "Agenda en kaart van van alle Repair Cafés in Amsterdam. Zie duidelijk wanneer ze open zijn en waar ze te bezoeken zijn",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#2D2E82",
+    icons: [
+      {
+        src: "/icon.svg",
+        sizes: "any",
+      },
+    ],
+  };
+}
