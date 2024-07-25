9443612a7590dcd28d8f99c4652f1a6c92a72f9a
peteruithoven <peter@metabolic.nl>
2024-07-25 20:21:17 +0200

    Hide summary arrow in Safari

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 export default function Summary({ children }: { children: string }) {
   return (
-    <summary className="-mx-3 flex cursor-pointer gap-2 bg-blue px-3 py-2 text-white">
+    <summary className="-mx-3 flex cursor-pointer gap-2 bg-blue px-3 py-2 text-white [&::-webkit-details-marker]:hidden [&::marker]:hidden">
       <ChevronRight className="shrink-0 transition-transform duration-100 ease-out group-open:rotate-90" />
       <Header className="m-0 text-white">{children}</Header>
     </summary>
