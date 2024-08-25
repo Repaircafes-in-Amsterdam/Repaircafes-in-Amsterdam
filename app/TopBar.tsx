90f6e35c3dbf57ae7bb584bcc3d0eb916b33dbfe
peteruithoven <peter@metabolic.nl>
2024-08-25 02:10:26 +0200

    Add desktop language chooser

--- app/TopBar.tsx
+++ app/TopBar.tsx
@@ -6,6 +6,7 @@ import MobileMenu from "./MobileMenu";
 import { useState } from "react";
 import classes from "./utils/classes";
 import Menu from "./Menu";
+import LocaleSelect from "./components/LocaleSelect";
 
 export default function TopBar() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
@@ -20,11 +21,14 @@ export default function TopBar() {
           )}
           onClick={() => setMenuIsOpen(false)}
         ></div>
-        <div className="flex flex-col gap-2.5 md:grow md:flex-row md:items-end md:justify-between">
+        <div className="flex flex-col gap-2.5 md:grow md:flex-row md:justify-between">
           <Link href="/" aria-label="Logo">
             <Logo />
           </Link>
-          <Menu />
+          <div className="flex flex-col justify-between md:items-end">
+            <LocaleSelect className="hidden md:block" />
+            <Menu />
+          </div>
         </div>
         <button
           onClick={() => setMenuIsOpen(!menuIsOpen)}
