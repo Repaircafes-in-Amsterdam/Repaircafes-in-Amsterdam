7566a64321a9c9492b39022549b0cc452034540e
peteruithoven <peter@metabolic.nl>
2024-08-28 14:58:00 +0200

    Remove tiny light blue line below FAQ details

--- app/components/Details.tsx
+++ app/components/Details.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react";
 
 export default function Details({ children }: { children: ReactNode }) {
   return (
-    <details className="group mb-3 bg-blue-250 px-3 pb-0.5">{children}</details>
+    <details className="group mb-3 overflow-hidden bg-blue-250 px-3">
+      {children}
+    </details>
   );
 }
