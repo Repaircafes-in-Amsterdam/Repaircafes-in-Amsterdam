db1ab3c7f6daff16a69d7053e05860296146ff4f
peteruithoven <peter@metabolic.nl>
2024-10-20 19:08:39 +0200

    Add title to videos

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -1,8 +1,9 @@
-export default function Video({ src }: { src: string }) {
+export default function Video({ src, title }: { src: string; title: string }) {
   return (
     <div className="aspect-h-9 aspect-w-16">
       <iframe
         src={src}
+        title={title}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;"
         allowFullScreen
         className="border-0"
