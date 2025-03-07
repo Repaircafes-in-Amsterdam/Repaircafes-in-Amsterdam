d5d45b34a61bdebe3dfd286e615ef66853bde8b1
peteruithoven <peter@metabolic.nl>
2025-03-07 11:07:46 +0100

    Include timezone offset

--- app/[locale]/(with-map)/festival/[slug]/getFestivalJsonLd.ts
+++ app/[locale]/(with-map)/festival/[slug]/getFestivalJsonLd.ts
@@ -1,5 +1,33 @@
+import { TIME_ZONE } from "@/app/constants";
 import { Festival } from "@/app/types";
 import { Event, WithContext } from "schema-dts";
+
+function getTime(partialTime: string): string {
+  return `${partialTime}:00`;
+}
+
+function getOffsetForTimeZone(date: Date): string {
+  // Since the timezone offset depends on daylight saving time we need to retrieve it dynamically
+  // Uses workaround with Intl.DateTimeFormat
+  // TODO Use Temporal when available
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    timeZone: TIME_ZONE,
+    timeZoneName: "short",
+  });
+  const parts = formatter.formatToParts(date);
+  const tzNamePart = parts.find((part) => part.type === "timeZoneName");
+  if (!tzNamePart) return "";
+  const offsetHours = tzNamePart.value.slice(-1);
+  return `${offsetHours}:00`;
+}
+
+function getDateTime(date: string, time: string): string {
+  const dateTime = new Date(`${date}T${getTime(time)}`);
+  const offset = getOffsetForTimeZone(dateTime);
+  const result = `${date}T${getTime(time)}+${offset}`;
+  console.log("date time: ", result);
+  return result;
+}
 
 export default function getFestivalJsonLd(
   festival: Festival,
@@ -14,12 +42,15 @@ export default function getFestivalJsonLd(
       "@type": "Schedule",
       startDate: festival.dates[0],
       endDate: festival.dates[festival.dates.length - 1],
-      startTime: `${festival.startTime}:00`,
-      endTime: `${festival.endTime}:00`,
+      startTime: getTime(festival.startTime),
+      endTime: getTime(festival.endTime),
       scheduleTimezone: "Europe/Amsterdam",
     },
-    startDate: `${festival.dates[0]}T${festival.startTime}:00`,
-    endDate: `${festival.dates[festival.dates.length - 1]}T${festival.endTime}:00`,
+    startDate: getDateTime(festival.dates[0], festival.startTime),
+    endDate: getDateTime(
+      festival.dates[festival.dates.length - 1],
+      festival.endTime,
+    ),
     location: {
       "@type": "Place",
       name: festival.location,
--- app/constants.ts
+++ app/constants.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = "https://repaircafe.amsterdam/";
+export const TIME_ZONE = "Europe/Amsterdam";
