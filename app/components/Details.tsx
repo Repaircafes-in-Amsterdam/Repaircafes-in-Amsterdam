67cf1848f111da06b8135e01d18a651cc38d1f6e
Peter Uithoven <peter@peteruithoven.nl>
2025-04-06 19:20:40 +0200

    Improve details style (#20)
    Co-authored-by: peteruithoven <peter@metabolic.nl>

--- app/components/Details.tsx
+++ app/components/Details.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Details({ children }: { children: ReactNode }) {
   return (
-    <details className="group bg-blue-250 mb-3 overflow-hidden px-3">
+    <details className="group bg-blue-250 mb-3 px-3 [&>p:first-of-type]:mt-2 [&>p:last-child]:pb-2">
       {children}
     </details>
   );
