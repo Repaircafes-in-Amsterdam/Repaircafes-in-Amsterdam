23acc8c1d2dcae30cd6f685bf1fcb192c54c6c86
peteruithoven <peter@metabolic.nl>
2026-03-28 22:17:42 +0100

    Support @ in unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
+import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "."),
+    },
+  },
   test: {
     environment: "jsdom",
   },
